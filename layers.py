46a3d95fc564e45bf5926113079d5136004d8d58
James Fitzpatrick <fitzpaj5@tcd.ie>
2018-08-07 22:20:50 +0100

    reshaping and capsule convolution added

--- layers.py
+++ layers.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as fn
 
+import numpy as np
+
 
 def compute_padding(in_dim, out_dim, ker_size, stride):
     numerator = ker_size - in_dim + stride * (out_dim - 1)
@@ -10,9 +12,11 @@ def compute_padding(in_dim, out_dim, ker_size, stride):
 
 def set_padding(in_dim, out_dim, ker_size, stride, mode):
     if not isinstance(mode, str):
-        raise TypeError("Padding must be a string of either same or none")
+        raise TypeError("Padding must be a string of same, half or none")
     if mode == 'same':
         padding = compute_padding(in_dim, in_dim, ker_size, stride)
+    elif mode == 'half':
+        padding = compute_padding(in_dim, in_dim / 2, ker_size, stride) + 1
     else:
         padding = 0
     return padding
@@ -32,8 +36,6 @@ class Convolve(nn.Module):
         self.stride = stride
 
         self.padding = set_padding(in_maps, out_maps, ker_size, stride, mode)
-
-        print(self.padding)
         
         self.conv = nn.Conv2d(
             in_channels=self.in_maps,
@@ -68,7 +70,7 @@ class Downsample(nn.Module):
         return self.down(tensor)
     
 
-class InitialCaps():
+class InitialCaps(nn.Module):
     def __init__(self, in_maps, out_maps, ker_size, num_caps):
         super(InitialCaps, self).__init__()
 
@@ -83,10 +85,49 @@ class InitialCaps():
                              mode='same')
 
     def forward(self, tensor):
-        return self.squash(self.conv(tensor))
+        tensor = self.conv(tensor)
+        tensor = self.squash(tensor)
+        return tensor
         
     def squash(self, tensor):
         norm = torch.norm(tensor, dim=0)
         numerator = (norm ** 2) * tensor
         denominator = (1 + norm ** 2) * norm
         return numerator / denominator
+
+
+class DownCaps(nn.Module):
+    def __init__(self, in_maps, out_maps, ker_size, mode,
+                 stride, num_caps, num_routes, num_atoms):
+        super(DownCaps, self).__init__()
+
+        self.in_maps = in_maps
+        self.out_maps = out_maps
+        self.ker_size = ker_size
+        self.mode = mode
+        self.stride = stride
+        self.num_caps = num_caps
+        self.num_routes = num_routes
+        self.num_atoms = num_atoms
+
+        self.convs = nn.ModuleList([
+            Convolve(
+                in_maps=self.in_maps,
+                out_maps=self.out_maps,
+                ker_size=self.ker_size,
+                stride=self.stride,
+                mode=self.mode) for _ in range(num_atoms)])
+
+    def forward(self, tensor):
+        tensors = [conv(tensor) for conv in self.convs]
+        tensors = torch.stack(tensors, dim=1)
+        return torch.Tensor.squeeze(tensors)
+
+
+def test_shape():
+    x = torch.tensor(np.zeros((1, 1, 512, 512))).float()
+    operation_one = Convolve(1, 16, 5, 1, 'same')
+    y = operation_one(x)
+    operation_two = DownCaps(16, 16, 5, 'half', 2, 256 * 256, 1, 2)
+    z = operation_two(y)
+    return z
