6457445de4db729407267dc5e6dbd0dba5d7fddb
James Fitzpatrick <fitzpaj5@tcd.ie>
2018-08-19 22:12:46 +0100

    cleaned up layers code

--- layers.py
+++ layers.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as fn
 
-import numpy as np
-
 import caps_utils
 
 
@@ -17,7 +15,8 @@ class Convolve(nn.Module):
         self.ker_size = ker_size
         self.stride = stride
 
-        self.padding = set_padding(in_maps, out_maps, ker_size, stride, mode)
+        self.padding = caps_utils.set_padding(
+            in_maps, out_maps, ker_size, stride, mode)
         
         self.conv = nn.Conv2d(
             in_channels=self.in_maps,
@@ -57,10 +56,10 @@ class DownCaps(nn.Module):
                 mode=self.mode) for _ in range(num_atoms)])
 
         self.weights = torch.rand(self.ker_height,
-                       self.ker_width,
-                       self.in_maps,
-                       self.num_atoms,
-                       self.out_maps).float()
+                                  self.ker_width,
+                                  self.in_maps,
+                                  self.num_atoms,
+                                  self.out_maps).float()
 
     def forward(self, tensor):
         votes = self.voting(tensor, self.weights)
@@ -86,7 +85,7 @@ class DownCaps(nn.Module):
             logits = fn.softmax(logits, dim=0)
             preds = torch.einsum('abcde,abde->bcde', (votes, logits))
             preds = self.squash(preds, dim=0)
-            logits += torch.einsum('abcde,bcde->abde', (votes, preds))
+            logits = logits + torch.einsum('abcde,bcde->abde', (votes, preds))
         return preds
 
 
@@ -116,10 +115,10 @@ class UpCaps(nn.Module):
                 mode=self.mode) for _ in range(num_atoms)])
 
         self.weights = torch.randn(self.ker_height,
-                       self.ker_width,
-                       self.in_maps,
-                       self.num_atoms,
-                       self.out_maps).float()
+                                   self.ker_width,
+                                   self.in_maps,
+                                   self.num_atoms,
+                                   self.out_maps).float()
 
     def forward(self, tensor):
         votes = self.voting(tensor, self.weights)
@@ -145,44 +144,81 @@ class UpCaps(nn.Module):
             logits = fn.softmax(logits, dim=0)
             preds = torch.einsum('abcde,abde->bcde', (votes, logits))
             preds = self.squash(preds, dim=0)
-            logits += torch.einsum('abcde,bcde->abde', (votes, preds))
+            logits = logits + torch.einsum('abcde,bcde->abde', (votes, preds))
         return preds
 
 
-def test_shape(in_tensor):
-    convolution_one = Convolve(1, 16, 5, 1, 'same')
-    a = convolution_one(in_tensor)
-    downsampling_one = DownCaps(16, 16, 5, 'same', 2, 256 * 256, 1, 2)
-    b = downsampling_one(a)
-    capsuling_one = DownCaps(16, 16, 5, 'same', 1, 256 * 256, 3, 4)
-    c = capsuling_one(b)
-    downsampling_two = DownCaps(16, 32, 5, 'same', 2, 128 * 128, 3, 4)
-    d = downsampling_two(c)
-    capsuling_two = DownCaps(32, 32, 5, 'same', 1, 128 * 128, 3, 8)
-    e = capsuling_two(d)
-    downsampling_three = DownCaps(32, 64, 5, 'same', 2, 64 * 64, 3, 8)
-    f = downsampling_three(e)
-    capsuling_three = DownCaps(64, 64, 5, 'same', 1, 64 * 64, 3, 8)
-    g = capsuling_three(f)
-    capsuling_four = DownCaps(64, 32, 5, 'same', 1, 64 * 64, 3, 8)
-    h = capsuling_four(g)
-    upsampling_one = UpCaps(32, 32, 5, 'none', 1, 128 * 128, 3, 8)
-    i = upsampling_one(h)
-    capsuling_five = DownCaps(32, 32, 5, 'same', 1, 128 * 128, 3, 8)
-    j = capsuling_five(torch.cat((e, i)))
-    capsuling_six = DownCaps(32, 32, 5, 'same', 1, 128 * 128, 3, 4)
-    k = capsuling_six(j)
-    upsampling_two = UpCaps(32, 16, 5, 'none', 1, 256 * 256, 3, 8)
-    l = upsampling_two(k)
-    capsuling_seven = DownCaps(16, 16, 5, 'same', 1, 256 * 256, 3, 4)
-    m = capsuling_seven(torch.cat((c, l)))
-    capsuling_eight = DownCaps(16, 16, 5, 'same', 1, 256 * 256, 3, 4)
-    n = capsuling_eight(m)
-    upsampling_two = UpCaps(16, 16, 5, 'none', 1, 512 * 512, 3, 2)
-    o = upsampling_two(n)
-    capsuling_nine = DownCaps(16, 16, 5, 'same', 1, 512 * 512, 3, 2)
-    p = capsuling_nine(torch.cat((a, o)))
-    capsuling_ten = DownCaps(16, 16, 5, 'same', 1, 512 * 512, 3, 1)
-    q = capsuling_ten(p)
+class CapsuleNetwork(nn.Module):
+    def __init__(self, ops, in_dims, out_dims, strides, routes, atoms, cats):
+        super(CapsuleNetwork, self).__init__()
+
+        self.ops = ops
+        self.in_dims = in_dims
+        self.out_dims = out_dims
+        self.strides = strides
+        self.routes = routes
+        self.atoms = atoms
+        self.cats = cats
+
+        self.ordered_ops = []
+
+        for op, in_dim, out_dim, stride, route, atom in zip(
+                self.ops,
+                self.in_dims,
+                self.out_dims,
+                self.strides,
+                self.routes,
+                self.atoms):
+            if op == Convolve:
+                self.ordered_ops = self.ordered_ops.append(
+                    op(in_dim, out_dim, 5, stride, 'same'))
+            if op == DownCaps:
+                self.ordered_ops = self.ordered_ops.append(
+                    op(in_dim, out_dim, 5, 'same', stride, 0, route, atom))
+            if op == UpCaps:
+                self.ordered_ops = self.ordered_ops.append(
+                    op(in_dim, out_dim, 5, 'same', stride, 0, route, atom))
+
     
-    return q
+class TestNetwork(nn.Module):
+    def __init__(self):
+        super(TestNetwork, self).__init__()
+
+        self.convolution_one = Convolve(1, 8, 5, 1, 'same')
+        self.downsampling_one = DownCaps(8, 8, 5, 'same', 2, 256 * 256, 1, 2)
+        self.capsuling_one = DownCaps(8, 8, 5, 'same', 1, 256 * 256, 3, 4)
+        self.downsampling_two = DownCaps(8, 16, 5, 'same', 2, 128 * 128, 3, 4)
+        self.capsuling_two = DownCaps(16, 16, 5, 'same', 1, 128 * 128, 3, 8)
+        self.downsampling_three = DownCaps(16, 32, 5, 'same', 2, 64 * 64, 3, 8)
+        self.capsuling_three = DownCaps(32, 32, 5, 'same', 1, 64 * 64, 3, 8)
+        self.capsuling_four = DownCaps(32, 16, 5, 'same', 1, 64 * 64, 3, 8)
+        self.upsampling_one = UpCaps(16, 16, 5, 'none', 1, 128 * 128, 3, 8)
+        self.capsuling_five = DownCaps(16, 16, 5, 'same', 1, 128 * 128, 3, 8)
+        self.capsuling_six = DownCaps(16, 16, 5, 'same', 1, 128 * 128, 3, 4)
+        self.upsampling_two = UpCaps(16, 8, 5, 'none', 1, 256 * 256, 3, 8)
+        self.capsuling_seven = DownCaps(8, 8, 5, 'same', 1, 256 * 256, 3, 4)
+        self.capsuling_eight = DownCaps(8, 8, 5, 'same', 1, 256 * 256, 3, 4)
+        self.upsampling_three = UpCaps(8, 8, 5, 'none', 1, 512 * 512, 3, 2)
+        self.capsuling_nine = DownCaps(8, 8, 5, 'same', 1, 512 * 512, 3, 2)
+        self.capsuling_ten = DownCaps(8, 1, 5, 'same', 1, 512 * 512, 3, 1)
+
+    def forward(self, tensor):
+        a = self.convolution_one(tensor)
+        b = self.downsampling_one(a)
+        c = self.capsuling_one(b)
+        d = self.downsampling_two(c)
+        e = self.capsuling_two(d)
+        f = self.downsampling_three(e)
+        g = self.capsuling_three(f)
+        h = self.capsuling_four(g)
+        i = self.upsampling_one(h)
+        j = self.capsuling_five(torch.cat((e, i)))
+        k = self.capsuling_six(j)
+        ll = self.upsampling_two(k)
+        m = self.capsuling_seven(torch.cat((c, ll)))
+        n = self.capsuling_eight(m)
+        o = self.upsampling_three(n)
+        p = self.capsuling_nine(torch.cat((a, o)))
+        q = self.capsuling_ten(p)
+        print('Inference completed...')
+        return q
